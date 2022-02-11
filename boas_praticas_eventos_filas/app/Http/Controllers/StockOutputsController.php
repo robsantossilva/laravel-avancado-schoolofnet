72652de9c87b5832fa943bd741095abfd2213594
Robson Silva <robsantossilva@gmail.com>
2022-02-11 10:58:18 -0300

    boas praticas com eventos e filas

---
+++ boas_praticas_eventos_filas/app/Http/Controllers/StockOutputsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use App\StockOutput;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+
+class StockOutputsController extends Controller
+{
+    public function index()
+    {
+        $movements = StockOutput::all();
+        return view('stock-outputs.index', compact('movements'));
+    }
+
+    public function create()
+    {
+        $products = Product::all()->pluck('name', 'id');
+        return view('stock-outputs.create', compact('products'));
+    }
+
+    public function store(Request $request)
+    {
+        $data = Arr::except($request->all(), '_token');
+        StockOutput::forceCreate($data);
+        return redirect()->route('stock_outputs.index');
+    }
+}
